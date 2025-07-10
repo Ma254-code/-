74d3a821b3f639e65c203862817f8d8b4119d7f3
Ma254-code <marlon.albrecht.leu@gmail.com>
2025-07-10 21:22:39 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", () => {
-  const loader = document.getElementById("loader");
-  gsap.to(loader, { opacity: 0, duration: 1, onComplete: () => loader.style.display = "none" });
-});
-
-const burger = document.getElementById("burger");
-const navLinks = document.getElementById("nav-links");
-burger.addEventListener("click", () => navLinks.classList.toggle("hidden"));
